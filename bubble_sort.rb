d94a7e85a4742d2f6740ef1f7ecb4c9282e77ff6
Hピエトロ <awpak1208h@gmail.com>
2022-01-21 16:28:04 +0900

    ループ処理など修正

--- bubble_sort.rb
+++ bubble_sort.rb
@@ -1,16 +1,10 @@
 nums = [10, 8, 3, 5, 2, 4, 11, 18, 20, 33]
 
-c = 1
-while c > 0 do
-  c = 0
+nums.size.times do
   nums.each_with_index do |num, n|
     next if n == nums.size - 1
-    a = nums[n]
-    b = nums[n+1]
-    if a < b
-      nums[n] = b
-      nums[n+1] = a
-      c += 1
+    if nums[n] > nums[n+1]
+      nums[n], nums[n+1] = nums[n+1], nums[n]
     end
   end
 end
